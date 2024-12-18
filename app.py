128d526bd9bcd446fc9afd5b9d6fb67e4d4159c8
CristianIvanInatech <cristian.ivan@inatech.eu>
2024-12-18 12:18:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -14,5 +15,6 @@ def echo():
     return jsonify({"response": text})
 
 if __name__ == '__main__':
-    # Run the app on localhost:5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Use the PORT environment variable assigned by Heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
