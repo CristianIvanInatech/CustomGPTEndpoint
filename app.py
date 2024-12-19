07eb695ac633a60457b1287eef109b2942f5b35c
CristianIvanInatech <cristian.ivan@inatech.eu>
2024-12-19 11:48:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)  # Add this line to enable CORS for all routes
+CORS(app)  
 
 @app.route('/echo', methods=['GET'])
 def echo():
-    # Extract the 'text' parameter from the query string
+    
     text = request.args.get('text', default="No text provided")
     
-    # Print the text to the console
-    print(f"Received text: {text}")
     
-    # Return the same text back to the user
-    return jsonify({"response": text})
+    try:
+        with open('medical_details.txt', 'a', encoding='utf-8') as file:
+            file.write(text + '\n')
+    except Exception as e:
+        print(f"Error writing to file: {e}")
+        return jsonify({"error": "Could not save data"}), 500
+    
+
+    return jsonify({"response": "Datele au fost verificate"})
 
 if __name__ == '__main__':
-    # Use the PORT environment variable assigned by Heroku
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
